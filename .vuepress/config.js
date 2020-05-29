70772679dcb10b372a73114c8fb3588e676d4a41
Ian Flom <ianrflom@gmail.com>
2020-05-29 12:47:35 -0500

    begin documentation of aggregated data api

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,11 +18,11 @@ module.exports = {
 			'/quickstart/',
 			'/glossary/',
 			{
-				title: 'User data',
-				path: '/userdata/',
+				title: 'Data Access',
+				path: '/data/',
 				collapsable: false,
 				children: [
-					'/userdata/api/'
+					'/data/aggregated/'
 				]
 			}
 		],
