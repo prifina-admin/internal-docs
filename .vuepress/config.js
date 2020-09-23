baf5820ab89ed153072819c2edfb1edd94b724aa
Valto Loikkanen <valto@loikkanen.com>
2020-09-23 12:07:32 +0300

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,10 +5,10 @@ module.exports = {
 		logo: '/img/logo.svg',
 		nav: [
 			{ text: 'Prifina.com', link: 'https://www.prifina.com/' },
-			{ text: 'For Developers', link: 'https://dev.prifina.com/' },
 			{
 				text: 'Community',
 				items: [
+					{ text: 'For Developers', link: 'https://dev.prifina.com/' },
 					{ text: 'Github', link: 'https://github.com/libertyequalitydata' },
 					{ text: 'Slack', link: 'https://join.slack.com/t/libertyequalitydata/shared_invite/zt-ddr4t974-MCzsch4FSeux8DrFQ2atbQ' }
 				]
